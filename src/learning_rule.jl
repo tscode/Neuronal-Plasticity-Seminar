5084eb173b5a648dc0374563cfb6b7e7a4d84472
ngc92 <erik-schultheis@freenet.de>
2015-05-23 15:28:32 +0200

    types and learning_rule.jl

---
+++ src/learning_rule.jl
@@ -0,0 +1,39 @@
+include("types.jl")
+
+function step( rule::AbstractRule, net::AbstractNetwork)
+  error("not implemented")
+end
+
+type ForceRule <: AbstractRule
+  P::AAF             # P variable
+  α::Float64         # alpha parameter
+
+  function ForceRule(size::Int, α::Float64)
+     P = 1/α * eye(size)
+     return new( P, α )
+  end
+end
+
+function step( rule::ForceRule, net::AbstractNetwork )
+  k = rule.P*net.neuron_out       # helper var
+  c  = 1/(1 + net.neuron_out ⋅ k)               # helper var
+  update_P!(P, k, c)
+  #=P -= (c*k)*k'=#
+  e  = z-f(t)# TODO task
+  # Changing the weigths
+  net.ω_o -= (e*c)*k
+end
+
+#inside an external function for performance reasons
+function update_P!(P::Matrix{Float64}, k::Vector{Float64}, c::Float64)
+    # diagonal values
+    # Off diagonal values
+    # Summation order column-major friendly --> really saves time!
+    @inbounds for j in 1:size(P)[2]
+        @simd for i in 1:size(P)[1]
+            P[i,j] -= (c*k[i]*k[j])::Float64
+        end
+    end
+end
+
+end
---
+++ src/types.jl
@@ -0,0 +1,7 @@
+
+abstract AbstractNetwork
+abstract AbstractNeuron
+
+typealias AAF AbstractArray{Float64, 2}
+
+abstract AbstractRule
