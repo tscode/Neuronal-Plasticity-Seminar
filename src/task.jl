4c0734579511ac068d526bd793c504b81e352143
ngc92 <erik-schultheis@freenet.de>
2015-05-25 15:21:17 +0200

    random fluctuations for task

--- src/task.jl
+++ src/task.jl
@@ -4,19 +4,21 @@ type FunctionTask <: AbstractTask
   time::Float64 # current time -- does the task need this one?
   funcs::Array{Function} # collection of functions
   expected::Array{Float64} # function values -> these are the expected values
+  
+  fluctuations::Float64	# amount of random noise added to the data
 
-  function FunctionTask( funcs::Array{Function} )
-    new(0, funcs, zeros(length(funcs)))
+  function FunctionTask( funcs::Array{Function}; fluctuations = 0.0 )
+    new(0, funcs, zeros(length(funcs)), fluctuations)
   end
 end
 
-function FunctionTask( funcs... )
-    FunctionTask( [funcs...] )
+function FunctionTask( funcs...; fluctuations = 0.0 )
+    FunctionTask( [funcs...], fluctuations )
 end
 
 function prepare_task!( task::FunctionTask, time::Real ) # usually only called by teacher
   for i in 1:length(task.funcs)
-      task.expected[i] = task.funcs[i](time)
+      task.expected[i] = task.funcs[i](time) + randn() * task.fluctuations
   end
   task.time = time
 end
