8a78ce00c904e1ed688a662ea60867809a888dbf
Thomas Staudt <ts.staudt@yahoo.de>
2015-05-30 14:28:52 +0200

    resolved error in evaluate function

--- src/evaluator.jl
+++ src/evaluator.jl
@@ -4,26 +4,36 @@ include("recorder.jl")
 type Evaluator
   chunksize::Int
   timeshift::Float64
+  function Evaluator(chunksize::Integer, timeshift::Real=0.)
+      return new(chunksize, timeshift)
+  end
 end
 
-function evaluate(evl::Evaluator, net::AbstractNetwork, task::AbstractTask, duration::Real)
+function evaluate(evl::Evaluator, net::AbstractNetwork, task::AbstractTask, duration::Real; rec::Bool=false, recorder=REC)
   start_time = net.time
   total_corr = 0.0
   step_count = 0
   while net.time < start_time + duration
     expected = zeros( evl.chunksize, size(net.output,1) )
     received = zeros( evl.chunksize, size(net.output,1) )
-    @rec net.time net.output[1] for i = 1:evl.chunksize
+    for i = 1:evl.chunksize
       update!(net)
       prepare_task!(task, net.time - evl.timeshift)
       expected[i,:] = get_expected(task)
       received[i,:] = net.output
+
+      if rec
+          record(recorder, 1, net.time)
+          for i in 1:length(net.output)
+              record(recorder, i+1, net.output[i])
+          end
+      end
     end
 
     # now evaluate the collected chunk.
     # first, we remove a costant phase shift. to do that, we look at the crosscorrelation of expected and received
     # and choose Δt such that it is maximized
-    mxsum = -inf(1.0)
+    mxsum = -Inf
     dtime = 0
     for ΔT = -div(evl.chunksize, 10):div(evl.chunksize, 10)
         summed = 0.0
