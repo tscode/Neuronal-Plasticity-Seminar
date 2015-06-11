b0a1a2e88db87b46a4f60def081faf4a9e837937
ngc92 <erik-schultheis@freenet.de>
2015-06-11 17:13:21 +0200

    test fitness for generator

--- src/fitness.jl
+++ src/fitness.jl
@@ -23,3 +23,27 @@ function test_fitness_for_task(net::AbstractNetwork, rule::AbstractRule, task::A
 
   return quality, evl.timeshift
 end
+
+
+# test fitness for a generator
+function test_fitness_of_generator(gen::AbstractGenerator; rnd::AbstractRNG=MersenneTwister(), samples = 25, threshold = 0.95)
+  mean_q = 0.0
+  mean_s = 0.0
+  success = 0
+  rule = ForceRule( gen.size, 0.1 )
+  for i = 1:samples
+    task = make_periodic_function_task(gen.num_output, [x->0], rnd)
+    net = generate( gen, int(rand()*10000))
+    q, s = test_fitness_for_task( net, rule, task )
+    if q > threshold
+      # rescale q reltive to threshold
+      mean_q += (q - threshold) / (1.0 - threshold)
+      mean_s += s
+      success += 1
+    end
+  end
+
+  mean_q /= success
+  mean_s /= success
+  return success/samples, mean_q, mean_s
+end
