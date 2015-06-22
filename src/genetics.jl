3a37a8d8f81ba2d8de26e14f6a8bc6e1b3668c31
ngc92 <erik-schultheis@freenet.de>
2015-06-22 19:36:24 +0200

    included first version of recombination

--- src/genetics.jl
+++ src/genetics.jl
@@ -10,7 +10,8 @@ type GeneticOptimizer{T, S <: AbstractSuccessRating}
   compare::Function # (S, S) → bool : compares two fitness measures, returns True if first arg is better than second
 
   generation::Int   # Counts the number of generations that have been calculated
-  recorder::Recorder
+  recorder::Recorder        # this recorder records parameters and success for each network in a generation as
+                            # key 1 and the means as key 2
   function GeneticOptimizer(fitness::Function, compare::Function;
                             population::Vector{T}=T[], success::Vector{S}=S[], seed::Int=0)
       new(MersenneTwister(seed), population, success, fitness, compare, 0, Recorder())
@@ -25,7 +26,7 @@ end
   #=GeneticOptimizer{T, S}(MersenneTwister(), T[], S[], fitness, compare)=#
 #=end=#
 
-function rate_population_parallel{T, S}(opt::GeneticOptimizer{T, S}; seed::Integer=randseed())
+function rate_population_parallel{T, S}(opt::GeneticOptimizer{T, S}; seed::Integer=randseed(opt.rng))
   rng = MersenneTwister(seed)
   gene_tuples = [ (gene, randseed(rng)) for gene in opt.population ]
   opt.success = collect(pmap(x -> opt.fitness(x[1], rng=MersenneTwister(x[2])), gene_tuples))
@@ -49,10 +50,7 @@ function init_population!{T,S}( opt::GeneticOptimizer{T, S}, base_element::T, N:
   end
 
   # calculate initial fitness
-  #=for i in 1:length(opt.population)=#
-    #=opt.success[i] = opt.fitness(opt.population[i])=#
-  #=end=#
-  opt.success = rate_population_parallel(opt, seed=randseed(opt.rng))
+  opt.success = rate_population_parallel(opt)
 end
 
 function step!( opt::GeneticOptimizer )
@@ -77,31 +75,43 @@ function step!( opt::GeneticOptimizer )
   end
 
   # TODO better way to save these values
-  success = [0.0,0.0,0.0]
+  means = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
   lidx = 1
   # replace dead entities
   for i = 1:length(opt.population)
     # alive index is the same as population index
     if !alive[i]
       # for now, only mutate: rewirte popultion[i] (which is not alive) with a mutation of living[lidx] (which was a good net)
-      new_generator = mutate!(opt, opt.population[i], opt.population[living[lidx]])
+      mutate!(opt, opt.population[i], opt.population[living[lidx]])
       lidx += 1
     else
-      success[1] += opt.success[i].quota
-      success[2] += opt.success[i].quality
-      success[3] += opt.population[i].size
-      record(opt.recorder, 1, [opt.generation, i , opt.success[i].quota, opt.success[i].quality, opt.population[i].size, opt.population[i].p, opt.population[i].gain, opt.population[i].feedback ])
+      data = [opt.success[i].quota, opt.success[i].quality, opt.population[i].size, opt.population[i].p, opt.population[i].gain, opt.population[i].feedback]
+      means += data
+      record(opt.recorder, 1, [opt.generation, i , data... ])
     end
   end
-  println(opt.success)
-  opt.success = rate_population_parallel(opt, seed=randseed(opt.rng))
+  # record means
+  record( opt.recorder, 2, 2*means / length(opt.population) )
 
-  # success measure
-  println(2*success/length(opt.population))
+  # re-evaluate population
+  opt.success = rate_population_parallel(opt, seed=randseed(opt.rng))
 
   opt.generation += 1
 end
 
+function calculate_next_generation!( opt::GeneticOptimizer, parents::Vector{AbstractGenerator}, targets::Vector{AbstractGenerator})
+  lidx = 1
+  for t in targets
+    if randbool(opt.rng)
+      t = mutate!(opt, t, parents[lidx])
+    else
+      t = recombine!(opt, t, parents[lidx], parents[lidx % length(parents) + 1])
+    end
+
+    # make lidx round-trip, so this works even if we require no relation between parents and targets sizes
+    lidx = lidx % length(parents) + 1
+  end
+end
 
 function mutate!( opt::GeneticOptimizer, target::AbstractGenerator, source::AbstractGenerator )
   # load parameters
@@ -111,6 +121,24 @@ function mutate!( opt::GeneticOptimizer, target::AbstractGenerator, source::Abst
   # change a single parameter
   pidx = convert(Int, round(rand(opt.rng) * length(parray) + 0.5))
   params[parray[pidx][1]] = random_param(parray[pidx][2], 9, rng=opt.rng)
+  import_params!( target, params )
+end
+
+function recombine!( opt::GeneticOptimizer, target::AbstractGenerator, A::AbstractGenerator, B::AbstractGenerator )
+  # combine A and B as parents for a new Generator target
+  # load parameters
+  params = export_params( target )
+  ap = export_params( A )
+  bp = export_params( B )
+  # randomize networks
+  for param : params
+    if( randbool(opt.rng) )
+      params[param[1]] = ap[param[1]]
+    else
+      params[param[1]] = bp[param[1]]
+    end
+  end
+
   import_params!( target, params )
 end
 
@@ -125,4 +153,5 @@ end
 
 function save_evolution(file, opt::GeneticOptimizer)
   writedlm(file, hcat(opt.recorder[1]...)')
+  writedlm("mean_"+file, hcat(opt.recorder[2]...)')
 end
