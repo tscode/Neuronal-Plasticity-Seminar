208a79a56cf92c8e99b066a0e95d993db9cfb34a
Thomas Staudt <ts.staudt@yahoo.de>
2015-07-31 19:20:35 +0200

    testfile commited

--- tests/understand_quality.jl
+++ tests/understand_quality.jl
@@ -1,20 +1,26 @@
-include("../src/EvoNet.jl")
+#=include("../src/EvoNet.jl")=#
+using EvoNet
 ev = EvoNet
-import EvoNet.@rec
 
-N = 10
+N = 100
 
-f(t) = cos(t) + 0.5sin(3t)
-g(t) = 0
-task = ev.FunctionTask( [f;], [g;] )
+ch = ev.simple_wave(n=1, amplitude=1, frequency=0.314/2pi, offset=0, phase=0)
+task = ev.get_task(ch)
+
 
 quals = Float64[]
-rule  = ev.ForceRule( N, 1 )
+rule  = ev.Learning.ForceRule( N, α=1/100 )
 
-generator = ev.SparseMatrixGenerator( N, 0.1, gain = 1.1  )
-for i in 1:10
-    net = ev.generate( generator, i )
-    push!( quals, ev.test_fitness_for_task(net, rule, task, 
-           learntime = 500, evaltime = 1000, waittime =  200, α=1/1000,
-           fname="")[1] )
-end
+ertop = ev.ErdösRenyiTopology(0.1)
+generator = ev.SparseFRGenerator( N, topology=ertop )
+
+
+net = ev.generate( generator, seed=ev.randseed() )
+ev.test_fitness_for_task(net, rule, task, fname="simu.dat")
+
+#=for i in 1:10=#
+    #=net = ev.generate( generator, i )=#
+    #=push!( quals, ev.test_fitness_for_task(net, rule, task, =#
+           #=learntime = 500, evaltime = 1000, waittime =  200, α=1/1000,=#
+           #=fname="")[1] )=#
+#=end=#
