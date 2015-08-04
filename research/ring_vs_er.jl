e5632a5a3c8787fd67407fc30aaf7f467e44d36b
Erik Schultheis <eriks@escala.nld.ds.mpg.de>
2015-08-04 14:37:54 +0200

    fix

--- research/ring_vs_er.jl
+++ research/ring_vs_er.jl
@@ -19,14 +19,14 @@ k = 2
 ertop = ev.ErdösRenyiTopology( p )
 ringtop = ev.RingTopology( k )
 # choose gain and feedback as in the paper
-gen  = ev.SparseFRGenerator( N, topology = ev.MetaTopology(ertop, ringtop), gain = 1.2, feedback = 2 )
+gen  = ev.SparseFRGenerator( N, topology = ev.MetaTopology([ertop, ringtop]), gain = 1.2, feedback = 2 )
 ev.init_population!(gopt, [(gen, 0.5)], 50)
 
 if N != 100
   info("this seems to be a test run. use N=100 to create final results")
 end
 
-for i = 1:25
+for i = 1:50
   ev.step!(gopt)
   ev.save_evolution("ring_vs_er.dat", gopt)
 end
