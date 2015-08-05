1fdb209581ed4f45c01b4fc48a1efd40d9ba752e
Thomas Staudt <ts.staudt@yahoo.de>
2015-08-05 14:08:33 +0200

    added q_of_omega_{unoptimized, high}.jl

---
+++ research/q_of_omega_unoptimized.jl
@@ -0,0 +1,23 @@
+push!(LOAD_PATH, "/scratch01/eriks/neural/Neuronal-Plasticity-Seminar/src/")
+
+using EvoNet
+#=ev = EvoNet=#
+
+# F_C(ω) curve for an unoptimized genotype
+
+@everywhere function rating(f)
+  N = 100 # use 100 for final run
+  p = 0.1
+  gain = 1.5
+  feedback = 2
+  ertop = EvoNet.ErdösRenyiTopology(p)
+  gen  = EvoNet.SparseFRGenerator( N, topology = ertop, gain = gain, feedback = feedback )
+  ch   = EvoNet.simple_wave(amplitude=1, frequency=f, offset=0)
+  env  = EvoNet.Environment(challenge=ch)
+  s = EvoNet.fitness_in_environment(gen, samples=200, env=env)
+  return EvoNet.get_value(s)
+end
+
+freq = logspace(-3, 0.5, 200)
+result = pmap(x->rating(x), freq)
+writedlm("freqrange_unoptimized.txt", [freq result])
